854a6b6a9cd1f8f5c5e80f4c56c4eb1bb832fb41
Ryan Wake <rwake345@gmail.com>
2020-09-28 15:49:50 -0400

    Custom logging
    Use Winston for custom logging

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 /* eslint-disable max-len */
+const {format, createLogger, transports} = require('winston');
+const {combine, timestamp, label, printf} = format;
 const DiscordBot = require('discord.js');
 const DiscordClient = new DiscordBot.Client();
 const config = require('./config.json');
@@ -8,13 +10,37 @@ if (config.dev) {
   require('dotenv').config();
 }
 
+/* Custom logging */
+const myFormat = printf(({level, message, label, timestamp}) => {
+  return `${timestamp} [${label}] ${level}: ${message}`;
+});
+
+const logger = createLogger({
+  level: 'info',
+  format: combine(
+      label({label: 'DiscordBot'}),
+      timestamp(),
+      myFormat,
+  ),
+  defaultMeta: {service: 'user-service'},
+  transports: [
+    new transports.File({filename: 'error.log', level: 'error'}),
+    new transports.File({filename: 'warn.log', level: 'warn'}),
+    new transports.File({filename: 'info.log', level: 'info'}),
+    new transports.File({filename: 'debug.log', level: 'debug'}),
+    new transports.File({filename: 'combined.log'}),
+  ],
+});
+
 /* Log errors and warnings to console. */
-DiscordClient.on('error', (error) => console.log(`Error: ${error}`));
-DiscordClient.on('warn', (info) => console.log(`Warning: ${info}`));
-DiscordClient.on('rateLimit', (rateLimitInfo) => console.log(`The bot has been rate limited, caused by ${rateLimitInfo.method}`));
-process.on('unhandledRejection', (error) => console.log(`Uncaught Promise Rejection: ${error}`));
+DiscordClient.on('error', (error) => logger.log('error', error));
+DiscordClient.on('warn', (info) => clogger.log('warn', info));
+DiscordClient.on('rateLimit', (rateLimitInfo) => logger.log('warn', `The bot has been rate limited, caused by ${rateLimitInfo.method}`));
+process.on('unhandledRejection', (error) => logger.log('error', `Uncaught Promise Rejection: ${error}`));
 
 /* Lets us know the bot is ready. */
-DiscordClient.on('ready', () => console.log('Bot ready and connected!'));
+DiscordClient.on('ready', () => {
+  logger.log('info', 'Bot ready and connected!');
+});
 
 DiscordClient.login(process.env.DISCORD_BOT_TOKEN || config.token);
